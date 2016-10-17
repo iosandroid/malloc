75a26d4ff2d5cf35b8edf17658678cba768e520b
Vadzim Ziankovich <v_ziankovich@wargaming.net>
2016-10-17 19:19:56 +0300

    [malloc]

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 //
 //
 
+struct m_block
+{
+	size_t   size;
+
+	m_block* next;
+	m_block* prev;
+};
+
+struct m_space
+{
+
+};
+
+void* m_alloc(size_t size)
+{
+
+}
+
+void m_free(void* p)
+{
+
+}
+
 void main()
 {
 
