563bc554a5201705e6f78258d9dd036c0cb59ffb
Vadzim Ziankovich <v_ziankovich@wargaming.net>
2016-10-20 19:34:30 +0300

    [malloc] added multithreading

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 //
 //
 
+#include <atomic>
 #include <stdint.h>
+#include <assert.h>
 #include <windows.h>
 //#include "malloc.c.h"
 
@@ -13,20 +15,42 @@ class m_allocator
 public:
 	static void* m_alloc(size_t size)
 	{
-		if (m_pool == 0)
-			m_pool = m_thread_local_init_pool();
+		if (m_map.thread_index == 0)
+		{
+			m_map.thread_index = ++m_map.thread_count;
+			assert(m_map.thread_index < MaxThreadCount);
+		}
 
-		return m_thread_local_alloc(m_pool, size);
+		p_thread_local_pool& pool = m_map.thread_pool[m_map.thread_index];
+		if (!pool)
+		{
+			pool = m_thread_local_init_pool();
+		}
+
+		return m_thread_local_alloc(m_map.thread_pool[m_map.thread_index], size);
 	}
 
 	static void m_free(void* p)
 	{
-		m_thread_local_free(m_pool, p);			
+		if (m_map.thread_index == 0)
+		{
+			m_map.thread_index = ++m_map.thread_count;
+			assert(m_map.thread_index < MaxThreadCount);
+		}
+
+		p_thread_local_pool pool = m_map.thread_pool[m_map.thread_index];
+		if (pool)
+		{
+			m_thread_local_free(pool, p);
+		}
 	}
 
 private:
-	static const size_t InUseBit     = 0x01;
-	static const size_t ListBinCount = 0x20;
+	using atomic_value = std::atomic < size_t >;
+
+	static const size_t InUseBit       = 0x01;
+	static const size_t ListBinCount   = 0x20;
+	static const size_t MaxThreadCount = 0x40;
 
 private:
 
@@ -36,7 +60,8 @@ private:
 
 	struct m_ctrl_block
 	{
-		size_t reserved;
+		size_t prev;
+		size_t head;
 	};
 	
 	/////////////////////////////////////////////////////////////////////////////////
@@ -72,6 +97,15 @@ private:
 		uint32_t     listmaps;
 		m_list_block listbins[ListBinCount];
 	};
+
+	/////////////////////////////////////////////////////////////////////////////////
+	struct m_thread_pool_map
+	{
+		atomic_value         thread_count;
+		thread_local(size_t) thread_index;
+
+		p_thread_local_pool  thread_pool[MaxThreadCount];
+	};
 	
 private:
 	static const size_t OneChunkSize = sizeof(m_ctrl_block);
@@ -105,7 +139,7 @@ private:
 			pool->listmaps = 0;
 
 			pool->head = add_blk(pool, sizeof(m_thread_local_pool));
-			pool->head->reserved = size - sizeof(m_thread_local_pool);
+			set_size(pool->head, size - sizeof(m_thread_local_pool));
 
 			// init list bins
 			for (size_t i = 0; i < ListBinCount; i++)
@@ -121,21 +155,23 @@ private:
 	{
 	}
 
-	static void* m_thread_local_alloc(p_thread_local_pool pool, size_t size)
+	static void* m_thread_local_alloc(p_thread_local_pool pool, size_t requested)
 	{
 		void* mem = NULL;		
-		if (size < MaxListBlockRequest)
+		if (requested < MaxListBlockRequest)
 		{
-			size_t request = (size < MinRequestSize) ? OneChunkSize : (size + OneChunkSize + 0x7) & ~0x7;
-			if (request < pool->head->reserved)
+			size_t size = (requested < MinRequestSize) ? OneChunkSize : (requested + OneChunkSize + 0x7) & ~0x7;
+			if (size < get_size(pool->head))
 			{
-				size_t residual_size = pool->head->reserved - request;
+				size_t rest = get_size(pool->head) - size;
 
 				p_ctrl_block p = pool->head;
-				p->reserved = request | InUseBit;
 
-				pool->head = add_blk(p, request);
-				pool->head->reserved = residual_size & ~InUseBit;
+				set_size(p, size);
+				set_inuse(p);
+
+				pool->head = add_blk(p, size);
+				set_size(pool->head, rest);
 
 				mem = blk_to_mem(p);
 			}
@@ -146,30 +182,35 @@ private:
 
 	static void m_thread_local_free(p_thread_local_pool pool, void* mem)
 	{
-		p_ctrl_block p = mem_to_blk(mem);
-		size_t size = blk_size(p);
+		p_ctrl_block curr = mem_to_blk(mem);
+		size_t size = get_size(curr);
 
-		p_ctrl_block n = add_blk(p, size);
-		if (!get_inuse(n))
+		p_ctrl_block next = add_blk(curr, size);
+		if (!get_inuse(next))
 		{
 
 		}
 	}
 
 private:
-	inline static size_t blk_size(p_ctrl_block blk)
+	inline static size_t get_size(p_ctrl_block blk)
 	{
-		return blk->reserved & ~InUseBit;
+		return blk->head & ~InUseBit;
+	}
+
+	inline static void set_size(p_ctrl_block blk, size_t size)
+	{
+		blk->head = size & ~InUseBit;
 	}
 
 	inline static bool get_inuse(p_ctrl_block blk)
 	{
-		return blk->reserved & InUseBit;
+		return blk->head & InUseBit;
 	}
 
-	inline static void set_inuse(p_ctrl_block blk)
+	inline static bool set_inuse(p_ctrl_block blk)
 	{
-		return blk->reserved | InUseBit;
+		return blk->head | InUseBit;
 	}
 
 	inline static void* blk_to_mem(p_ctrl_block p)
@@ -207,10 +248,11 @@ private:
 	}
 
 private:
-	thread_local(p_thread_local_pool) m_pool;
+	static m_thread_pool_map m_map;	
 };
 
-m_allocator::p_thread_local_pool m_allocator::m_pool = NULL;
+m_allocator::m_thread_pool_map m_allocator::m_map;
+size_t m_allocator::m_thread_pool_map::thread_index = 0;
 
 
 /////////////////////////////////////////////////////////////////////////////////////
